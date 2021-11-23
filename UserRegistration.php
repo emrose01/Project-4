7eb209eca9794cfcf3012de8e0c421044bb6ba88
CyKovachevich <89612240+CyKovachevich@users.noreply.github.com>
2021-11-23 11:55:25 -0600

    Update UserRegistration.php

--- UserRegistration.php
+++ UserRegistration.php
@@ -7,7 +7,25 @@ CSC 390
 Project 4 User Registration
 -->
 
-<?php
+<?php include_once 'header.php'; ?>
+
+    <body>
+        
+        <h3> Project 4 User Registration </h3>
+        <hr>
+        <p>
+            <br>
+            <form method = "post" action = "" >
+
+                Name: <input type = "text" name = "name"/> <br><br>
+                Email: <input type = "text" name = "email"/> <br><br>
+                Password: <input type = "text" name = "password"/> <br><br>
+
+                <button type = "submit" name = "submit">Register</button>
+            </form>
+        </p>
 
 
-
+        
+    </body>
+</html>
