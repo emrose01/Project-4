702c172a66c53e973d2f7bd0e9544cf46cbba860
CyKovachevich <89612240+CyKovachevich@users.noreply.github.com>
2021-11-23 12:44:48 -0600

    Update ViewList.php

--- ViewList.php
+++ ViewList.php
@@ -1,1 +1,23 @@
+<!--
+Emily Morton
+Cy Kovachevich
+Ryan Mills
+2 December 2021
+CSC 390
+Project 4 View List Page
+-->
 
+<?php include_once 'header.php'; ?>
+
+    <body>
+        
+        <h3> Project 4 To-Do List</h3>
+      
+      
+     </body
+      
+      
+      
+      </html>
+      
+      WORK IN PROGRESS
